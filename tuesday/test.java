4d53b093d60f9cf3f366330f0929a92add05a270
anand-san <anandk683@gmail.com>
2019-07-04 10:11:59 +0530

    new file:   tuesday/test.java

---
+++ tuesday/test.java
@@ -0,0 +1,9 @@
+public class test
+{
+	public static void main(String ... sdf)
+	{
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE);
+	}
+}
